1e1b5f0ca946e3fa0c9d04514bc9b97582c21953
Syrion <137480929+SyrionDevelopment@users.noreply.github.com>
2023-08-24 17:30:51 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Syrion Development'
+description 'Vehicle Blacklist Script'
+version '1.0.0'
+
+client_scripts {
+	"client/vehicles-client.lua",
+	"client/general-client.lua"
+}
